a725a3de16d7224d2b905e920edf39be51878687
Fakhri Nur Ramdhani <fakhrinurramdhani@Fakhris-MacBook-Air.local>
2026-05-30 22:57:20 +0700

    update data

--- Tubes.py
+++ Tubes.py
@@ -3,4 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_excel('jobSalary_Kel_Meteor_classified.xlsx')
-data.head()
+kolom_dipakai = [
+    "job_level",
+    "experience_years",
+    "education_num",
+    "skills_count",
+    "certifications",
+    "salary"
+]
+data = data[kolom_dipakai]
+print(data.head())
+print(data.describe())
